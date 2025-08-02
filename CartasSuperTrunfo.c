b2c5d4a9808d8b74066c3cdd6ec4ea7bb46f2b4f
LenonPrado <lenonprado021@gmail.com>
2025-08-02 14:18:27 +0000

    DesafioTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,11 @@
     float pib;
     int pontosTuristicos;
 
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
+
 int main() {
     
     printf("Preencha os dados da Carta Super Trunfo1:\n");
@@ -13,13 +18,29 @@ int main() {
     scanf("%d\n", &populacao);
 
     printf("Área (em km²):");
-    scanf("%f\n", &area);
+    scanf("%.2f km²\n", &area);
 
     printf("PIB (Bilhões de reais):");
-    scanf("%f\n", &pib);
+    scanf("%.2f bilhões\n", &pib);
 
     printf("Número de Pontos Turísticos:");
     scanf("%d\n", &pontosTuristicos);
+
+
+    printf("Preencha os dados da Carta Super Trunfo2:\n");
+
+    printf("População:");
+    scanf("%d\n", &populacao2);
+
+    printf("Área (em km²):");
+    scanf("%.2f km²\n", &area2);
+
+    printf("PIB (Bilhões de reais):");
+    scanf("%.2f bilhões\n", &pib2);
+
+    printf("Número de Pontos Turísticos:");
+    scanf("%d\n", &pontosTuristicos2);
+
 
    return 0; 
 };
